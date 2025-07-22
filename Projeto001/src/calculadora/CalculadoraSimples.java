2786d10cd11681d2f371bdd6f1a74008f75880f2
braya <braya@Brayan>
2025-07-22 11:25:32 -0300

    Tratamento de exceção basico

--- Projeto001/src/calculadora/CalculadoraSimples.java
+++ Projeto001/src/calculadora/CalculadoraSimples.java
@@ -1,45 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package calculadora;
 
 import java.util.Scanner;
 
 /**
  *
- * @author braya
+ * @author brayan
  */
 public class CalculadoraSimples {
 
     public CalculadoraSimples() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o primeiro numero para a operação");
-        double n1 = sc.nextDouble();
-        System.out.println("Digite o segundo numero para a operação");
-        double n2 = sc.nextDouble();
-        System.out.println("Escolha a operação desejada");
-        System.out.println("(1)soma\n(2)subtração\n(3)multiplicação\n(4)divisão");
-        int e = sc.nextInt();
-        switch (e) {
-            case 1:
-                System.out.println("Resultado: " + (n1 + n2));
-                break;
-            case 2:
-                System.out.println("Resultado: " + (n1 - n2));
-                break;
-            case 3:
-                System.out.println("Resultado: " + (n1 * n2));
-                break;
-            case 4:
-                System.out.println("Resultado: " + (n1 / n2));
-                break;
-            default:
-                System.out.println("Opção não listada");
-                System.out.println("Operação finalizada");
+        try {
+            System.out.println("Digite o primeiro numero para a operação");
+            double n1 = sc.nextDouble();
+            System.out.println("Digite o segundo numero para a operação");
+            double n2 = sc.nextDouble();
 
+            System.out.println("Escolha a operação desejada");
+            System.out.println("(1)soma\n(2)subtração\n(3)multiplicação\n(4)divisão");
+            int e = sc.nextInt();
+            switch (e) {
+                case 1:
+                    System.out.println("Resultado: " + (n1 + n2));
+                    break;
+                case 2:
+                    System.out.println("Resultado: " + (n1 - n2));
+                    break;
+                case 3:
+                    System.out.println("Resultado: " + (n1 * n2));
+                    break;
+                case 4:
+                    System.out.println("Resultado: " + (n1 / n2));
+                    break;
+                default:
+                    System.out.println("Opção não listada");
+                    System.out.println("Operação finalizada");
+            }
+        } catch (Exception e) {
+            System.out.println("Apenas numeros");
         }
-
     }
-
 }
